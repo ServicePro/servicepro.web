75170aed3af0fa27cb472c395ae3f074b83a822b
Ry_MAC <ryanjeong7@me.com>
2013-12-18 18:33:06 +0900

    1

--- servicePro/ry/script/index.js
+++ servicePro/ry/script/index.js
@@ -2,13 +2,28 @@
 
 $(document).ready(function(){
 	var tmpUser = navigator.userAgent;	
-	 
-$(window).resize(function() {
-        var wi = $(window).width();
-        $("#testp").text('Screen width is currently: ' + wi + 'px.');
-        $('#divWform_cat0_1').insertAfter($('#divWform_cat0_2'));
-   		$('#divWform_cat0_2').html($('#divWform_cat0_1').html());
-});
+//화면 사이즈에 따라 폼 위치 조	 
+	var divPosition = function(){
+		var wi = $(window).width();
+		$("#testp").text('Screen width is currently: ' + wi + 'px.');
+		var rowsize = 0;
+		if(wi < 750){
+			rowsize = 2;
+		}else if((wi < 970) && (wi > 750)){
+			rowsize = 3;
+		}else if(wi > 970){
+			rowsize = 6;
+		}
+		$(".divWform").each(function(){
+			var num = $(this).attr("id").split('_');
+			var item_num = num[2];
+			var row = Math.floor(item_num / rowsize) + 1;
+			lastblock = (row*rowsize) - 1;
+			$(this).insertAfter($("#block"+lastblock));
+		});
+	};
+
+	
 
 $('.cat0').on('click', function(){
 	if (tmpUser.indexOf("iPhone") > 0 || tmpUser.indexOf("iPod") > 0 || tmpUser.indexOf("Android ") > 0 )  {
@@ -19,22 +34,38 @@ $('.cat0').on('click', function(){
 });	
 
 var toggleMform = function(obj){
-	
-	
-		var formid = $(obj).attr("id");
-		$('#divMform_' + formid).toggle();
-	
-
+	//form toggle
+	var formid = $(obj).attr("id");
+	if($('#divMform_' + formid).is( ':visible' )){
+		$('#divMform_' + formid).hide();	
+	}else{
+		$(".divMform").each(function() {
+			if($(this).is( ':visible' )){
+				$(this).hide();
+			}
+		});
+		$('#divMform_' + formid).show();	
+	}
 };
 
 var toggleWform = function(obj){
-	var winWidth = $(window).width();
-	alert(winWidth);
-		var formid = $(obj).attr("id");
-		$('#divWform_' + formid).toggle();
-	
-
+	//form toggle
+	var formid = $(obj).attr("id");
+	if($('#divWform_' + formid).is( ':visible' )){
+		$('#divWform_' + formid).hide();	
+	}else{
+		$(".divWform").each(function() {
+			if($(this).is( ':visible' )){
+				$(this).hide();
+			}
+		});
+		$('#divWform_' + formid).show();	
+	}
 };
 	
-});
+	$( window ).resize(function() {
+	  divPosition();
+	});
+	divPosition();
 
+});
